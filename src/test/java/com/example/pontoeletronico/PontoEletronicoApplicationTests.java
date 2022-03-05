4a42bfa7b05b5d32be98099fdac2e06b668951b4
“charles <charlesbral78@hotmail.com>
2022-03-05 14:31:45 -0300

    Teste Tresvis CI

---
+++ src/test/java/com/example/pontoeletronico/PontoEletronicoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.pontoeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class PontoEletronicoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
